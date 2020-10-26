9881a4d466cb8a4e8f59a1d7ca33f67df2955e01
Göksu Ece Okur <goksueceokur@windowslive.com>
2020-10-27 00:30:15 +0300

    bug fix

--- AStardeneme2solve.py
+++ AStardeneme2solve.py
@@ -2,21 +2,21 @@ import AStardeneme2
 
 
 def branchAndBound(mostPromising):
+    mostPromising = queue[0]
     if mostPromising.current != goal_state:
         mostPromising.generateChild()
 
         for node in queue:
             node.prune()
     
-    else:
+    elif bestSoFar.steps > mostPromising.steps :
         bestSoFar = mostPromising
 
     if queue != []:
-        mostPromising = queue[1]
         return branchAndBound(mostPromising)
     
     else:
         return "best solution is found: " + bestSoFar
 
 
-mostPromising = queue[1]
+
