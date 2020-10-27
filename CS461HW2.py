70513956da0568c724f4073f2c166310376000f5
burakokumus <burakokumus98@gmail.com>
2020-10-27 12:24:44 +0300

    Update shuffle

--- CS461HW2.py
+++ CS461HW2.py
@@ -1,38 +1,25 @@
 import random
 
+NUM_OF_MOVES = 100000
+
 def shuffle(array):
-    j=0
-    while j != 10:
+    for i, row in enumerate(array):
+        if 0 in row:
+            blank_x, blank_y = i, row.index(0) # coordinates of the 0
+    
+    for j in range(NUM_OF_MOVES):
         chance = random.randint(1,4)
-        i = 0
-        blank = -1
 
-        while blank == -1:
-            Lists = array[i]
-
-            if 0 in Lists:
-                blank = Lists.index(0)
-
-            else:
-                i = i+1
-
-        if chance == 1 and blank != 0: 
-            Lists[blank], Lists[blank - 1] = Lists[blank - 1], Lists[blank]
-            j= j + 1
-
-        elif chance == 2 and blank != len(Lists) - 1: 
-            Lists[blank], Lists[blank + 1] = Lists[blank + 1], Lists[blank]
-            j= j + 1
-
-        elif chance == 3 and i != len(array) - 1 : 
-            array[i][blank], array[i + 1][blank] = array[i+1][blank], array[i][blank]
-            j= j + 1
-
-        elif chance == 4 and i != 0 : 
-            array[i][blank], array[i - 1][blank] = array[i-1][blank], array[i][blank]
-            j= j + 1
-            
-        # print("", array [0] , "\n" , array [1] , "\n" , array [2] ,  "\n" , array [3], "\n" )
+        if chance == 1 and blank_x != 0: # move blank left
+            array[blank_x][blank_y], array[blank_x - 1][blank_y] = array[blank_x - 1][blank_y], array[blank_x][blank_y]
+            blank_x -= 1
+        elif chance == 2 and blank_x != 3: # move blank right
+            array[blank_x][blank_y], array[blank_x + 1][blank_y] = array[blank_x + 1][blank_y], array[blank_x][blank_y]
+            blank_x += 1
+        elif chance == 3 and blank_y != 0: # move blank up
+            array[blank_x][blank_y], array[blank_x][blank_y - 1] = array[blank_x][blank_y - 1], array[blank_x][blank_y]
+        elif chance == 4 and blank_y != 3: # move blank down
+            array[blank_x][blank_y], array[blank_x][blank_y + 1] = array[blank_x][blank_y + 1], array[blank_x][blank_y]
     return array
         
         
@@ -43,4 +30,9 @@ goal_state = [[1, 2, 3, 4],
              [3, 4, 5, 5],
              [4, 5, 5, 0]]
 
-print(shuffle(goal_state))
+shuffled = shuffle(goal_state)
+for row in shuffled:
+    for i in row:
+        print(i, end='   ')
+
+    print()
