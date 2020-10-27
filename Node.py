4bf92dee124686ea9641d7d0b3b5eda9d8fdde56
Yüce <yuceyuce99@gmail.com>
2020-10-27 23:49:32 +0300

    Add comments

--- Node.py
+++ Node.py
@@ -1,39 +1,74 @@
+'''
+CS 461 - Artificial Intelligence - Fall 2020 - Homework 2
+Professor: Varol Akman
+Group Name: PROMINI
+Group Members:
+    -Hakkı Burak Okumuş
+    -Göksu Ece Okur
+    -Yüce Hasan Kılıç
+
+Node class represents a state for E-15 Puzzle.
+A state keeps current grid as a 5x5 matrix.
+A state keeps the previous states that leads to that state as a path.
+A state keeps an estimated total distance of the shortest path including that state.
+
+Formula for estimated total distance:
+    Estimated total distance = Steps taken so far + Estimated number of steps remaining
+
+Estimating is done using heuristics.
+Heuristic formula:
+    Underestimated number of remaining moves: number of misplaced tiles
+
+It is an admissible heuristic for the 15-puzzle, since every tile that is out of position
+must be moved at least once. In the best case, blank tile is moved towards the bottom-right corner without making loops
+and each move places one tile in its correct position. In other words, each misplaced tile is apart from its correct position
+by one position.
+
+Constraint for moving the blank tile
+    1. If blank tile is on the edge of a side, it cannot be moved towards that side.
+'''
+
 import random
+
+# Array representation of the goal state
 goal_state = [[1, 2, 3, 4],
              [2, 3, 4, 5],
              [3, 4, 5, 5],
              [4, 5, 5, 0]]
 
-NUM_OF_MOVES = 10
-
+# Number of moves to shuffle the path initially
+NUM_OF_MOVES = 10 
 
 class Node:
-
     def __init__( self, array, stepsTaken, path = []):
         self.current = [row[:] for row in array]
         self.steps = stepsTaken
         self.path = path
         self.estimation = self.estimate()
 
+    # Check whether state is in its path i.e path contains a loop
     def loop_check (self):
         if self.current in self.path:
             return True
     
+    # Shuffle the puzzle by making NUM_OF_MOVES random moves
     def shuffle(self):
         j = 0
-        while j < NUM_OF_MOVES: # move(self, array, blank_x, blank_y, direction)
+        while j < NUM_OF_MOVES:
             chance = random.randint(1,4)
 
+            # If move is valid, increase count; skip, otherwise
             if self.move(chance):
-                j += 1
+                j += 1 
+        # Update the estimated length of path after the shuffle
+        self.estimate()
 
-    def steps_taken (self):
-        self.steps = len(self.path)
-
+    # Estimates the total length of the path using heuristic formula discussed in the header
     def heuristic1 (self, goal_state):
         i = 0
         j = 0
         
+        # For each tile, compare it to the goal state and increment if it is misplaced.
         while j != 4:
             k = 0 
             while k != 4:
@@ -41,13 +76,16 @@ class Node:
                     i += 1
                 k += 1
             j += 1
+            
         return i
 
+    # Updates the estimation value of the state
     def estimate (self):
         self.estimation = self.heuristic1(goal_state) + self.steps
         return self.estimation
     
-
+    # Generate next possible states by moving the blank tile in possible directions.
+    # Returns list of generated children
     def generate_children(self):
         children = []
         for i in range(1, 5):
@@ -58,10 +96,12 @@ class Node:
                 children.append(new_node)
         return children
             
-    def move(self, direction): # 1: left, 2: right, 3: up, 4: down
+    # Moves the blank tile in given direction and updates the grid
+    # Directions => 1: left, 2: right, 3: up, 4: down
+    def move(self, direction): 
         for i, row in enumerate(self.current):
             if 0 in row:
-                blank_x, blank_y = i, row.index(0) # coordinates of the 0
+                blank_x, blank_y = i, row.index(0) # coordinates of the blank tile
         if direction == 1: # move blank left
             if blank_x == 0:
                 return False
@@ -86,10 +126,8 @@ class Node:
             self.current[blank_x][blank_y], self.current[blank_x][blank_y + 1] = self.current[blank_x][blank_y + 1], self.current[blank_x][blank_y]
             blank_y += 1
             return True
-    
-    def __eq__(self, other): # TODO
-        return
 
+    # Prints the states in the path and node itself at the end
     def print_path(self):
         res = ''
         for node in self.path:
@@ -106,11 +144,12 @@ class Node:
         res += '------------\n'
         print(res)
 
-
+    # Print the details of a single node
+    # Used for debugging purposes
     def __repr__(self):
         cur = ''
         for row in self.current:
             for x in row:
                 cur += (str(x) + ' ')
             cur += '\n'
-        return "\ncurrent node: \n" + cur +"\nsteps Taken so far: " + str(self.steps) + "\nLower Bound of current node: " + str(self.estimation)
+        return "\n" + cur +"\nsteps Taken so far: " + str(self.steps) + "\nLower Bound of current node: " + str(self.estimation)
--- Solver.py
+++ Solver.py
@@ -1,5 +1,31 @@
+'''
+CS 461 - Artificial Intelligence - Fall 2020 - Homework 2
+Professor: Varol Akman
+Group Name: PROMINI
+Group Members:
+    -Hakkı Burak Okumuş
+    -Göksu Ece Okur
+    -Yüce Hasan Kılıç
+
+Solver class represents the solver mechanism for E-15 Puzzle.
+Uses A* algorithm to solve the problem.
+Program creates an initially solved puzzle and shuffles it.
+Then, it creates all valid children (next states) from the first element of the queue and adds them to a queue in each step.
+
+Redundant paths are pruned.
+Pruning conditions:
+    1. Estimated distance is longer than upper bound.
+    2. State causes a loop.
+    3. State is already reached from a shorter path. 
+
+The queue is sorted based on estimated total path length of each state on each step.
+Tree data structure is used to represent paths and find a solution (the shortest one).
+Tries to reach the goal state from first element of the queue.
+'''
+
 from Node import Node
 
+# Array representation of the goal state
 goal_state = [ [1, 2, 3, 4],
                [2, 3, 4, 5],
                [3, 4, 5, 5],
@@ -8,34 +34,38 @@ goal_state = [ [1, 2, 3, 4],
 class Solver:
     def __init__(self):
         self.queue = []
-        root_node = Node( [row[:] for row in goal_state], 0)
-        root_node.shuffle() 
+        root_node = Node( [row[:] for row in goal_state], 0) # Pass the goal state to create an initial puzzle
+        root_node.shuffle() # Shuffle the puzzle
         self.queue.append(root_node) # Form one element queue consisting of a zero-length path containing only the root node
-        self.states_reached = []
+        self.states_reached = [] # Dynamic programming
         self.upper_bound = 1000 # Sufficiently big number
+        self.max_queue_size = 1 
 
-    #Sorts the entire queue by the sum of path length and a lower-bound estimate of the cost remaining, with least cost paths in front
+    # Sorts the entire queue by the sum of path length and a lower-bound estimate of the cost remaining, with least cost paths in front
     def sort_queue(self):
         self.queue.sort(key=lambda x: x.estimation, reverse=False)
 
+    # Removes redundant paths from the queue on conditions discussed in the header
     def prune (self):
         for node in self.queue:
             if node.loop_check() or self.already_reached(node) or self.worse_than_upper_bound(node):
                 self.queue.remove(node)
 
+    # Checks if the state is already reached with fewer steps (Dynamic Programming)
     def already_reached (self, newState):
         for state in self.states_reached:
             if newState.current == state.current:
-                if state.steps < self.steps:
+                if state.steps < self.upper_bound:
                     return True
                 else:
                     state = newState
         return False
 
+    # Checks if the path containing the state is estimated longer than current upper bound.
     def worse_than_upper_bound(self, node):
         return node.estimation > self.upper_bound
 
-    #CHANGE
+    # Implementation of A* algorithm using Branch and Bound and heuristics
     def solve(self):
 
         # Until the first path in the queue terminates at the goal node or the queue is empty,
@@ -46,6 +76,7 @@ class Solver:
             # If the first path in the queue terminates at the goal node, announce success
             if most_promising.current == goal_state:
                 most_promising.print_path()
+                print("Maximum size of queue", self.max_queue_size)
                 return True 
             
             # Create new paths by extending the first path to all the neighbors of the terminal node
@@ -62,6 +93,9 @@ class Solver:
             # Reject loops and remove redundant paths
             self.prune()
 
+            if(len(self.queue) > self.max_queue_size):
+                self.max_queue_size = len(self.queue)
+
             # Sort the entire queue by the sum of path length and a lower bound estimate of the cost remaining, with least cost paths in front
             self.sort_queue()
 
@@ -69,10 +103,6 @@ class Solver:
         return False 
 
 if __name__ == "__main__":
-
-    solver = Solver()
-    result = solver.solve()
-    if(result):
-        print("Problem solved")
-    else:
-        print("Problem cannot be solved")
+    puzzle = Solver()    
+    print("Solution for puzzle")
+    result = puzzle.solve()
